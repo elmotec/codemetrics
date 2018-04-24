013c1bff2aef369b5c21f0691c719a714f0c591c
elmotec <elmotec@gmx.com>
2018-04-23 22:45:09 -0400

    Added HotSpotReport
    Refactored common code in BaseReport. Extracted SvnLogCollector in its
    own class.

--- codemetrics.py
+++ codemetrics.py
@@ -9,13 +9,15 @@ import subprocess
 import logging
 import xml.etree.ElementTree as ET
 import datetime as dt
+import logging
+import csv
+import pathlib as pl
 
 import pandas as pd
 
 
-log = logging.getLogger()
+log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
-
 
 def get_now():
     """Get current time stamp as pd.TimeStamp.
@@ -38,127 +40,223 @@ def _run(command, **kwargs):
         kwargs['shell'] = True
     if 'universal_newlines' not in kwargs:
         kwargs['universal_newlines'] = True
-    return subprocess.check_output(command, **kwargs).split('\n')
+    try:
+        log.info(command)
+        return subprocess.check_output(command, **kwargs).split('\n')
+    except subprocess.CalledProcessError as err:
+        log.warning(err)
+        raise
 
 
-class _DfSpec:
-    """Specifies what the SCM DataFrame should look like."""
-    columns = ['revision', 'author', 'date', 'textmods', 'kind',
-               'action', 'propmods', 'path', 'message']
+class SvnLogCollector:
+    """Collect log from Subversion."""
 
+    def __init__(self, path, svn_program=None, after=None):
+        """Initialize."""
+        self.path = path
+        self.svn_program = svn_program or 'svn'
+        self.after = after
+
+    def get_relative_url(self):
+        """Relative URL so we can generate local paths."""
+        relative_url = None
+        for line in _run(f'{self.svn_program} info {self.path}'):
+            if line.startswith('Relative URL'):
+                relative_url = pl.Path(line.split(': ^')[1])
+                break
+        return relative_url
+
+    def process_entry(self, log_entry, relative_url):
+        """Convert a single xml <logentry/> element to csv rows.
+
+        :param str log_entry: <logentry/> element.
+        :param str relative_url: relative url for path elements.
+
+        :yield: one or more csv rows.
+
+        """
+
+        elem = ET.fromstring(log_entry)
+        revision = elem.attrib['revision']
+        values = {}
+        for sub in ['author', 'date', 'msg']:
+            try:
+                values[sub] = elem.find(f'./{sub}').text
+            except (AttributeError, SyntaxError):
+                log.warning('failed to retrieve %s in %s', sub, log_entry)
+                values[sub] = None
+        if values['msg']:
+            values['msg'] = values['msg'].replace('\n', ' ')
+        for path_elem in elem.findall('*/path'):
+            other = {}
+            for sub in ['text-mods', 'kind', 'action', 'prop-mods']:
+                try:
+                    other[sub] = path_elem.attrib[sub]
+                except (AttributeError, SyntaxError, KeyError):
+                    other[sub] = None
+            try:
+                path = str(pl.Path(path_elem.text).relative_to(relative_url))
+            except (AttributeError, SyntaxError, ValueError) as err:
+                log.warning(f'{err} processing revision {revision}')
+                path = None
+            row = (revision,
+                   values['author'],
+                   values['date'],
+                   other['text-mods'],
+                   other['kind'],
+                   other['action'],
+                   other['prop-mods'],
+                   path,
+                   values['msg'])
+            yield row
+
+    def process(self, xml, relative_url):
+        """Convert output of svn log --xml -v to a csv.
+
+        :param iter(str) xml: iterable of string (one for each line)
+
+        :return: iter(tuple) containing the parsed entries.
+
+        """
+        log_entry = ''
+        for line in xml:
+            if line.startswith('<logentry'):
+                log_entry += line
+                continue
+            if not log_entry:
+                continue
+            log_entry += line
+            if not line.startswith('</logentry>'):
+                continue
+            yield from self.process_entry(log_entry, relative_url)
+            log_entry = ''
+
+    def get_log(self):
+        """Call svn log --xml -v and return the output as a DataFrame."""
+        relative_url = self.get_relative_url()
+        command = f'{self.svn_program} log --xml -v '
+        if self.after:
+            command += '-r {' + self.after.strftime('%Y-%m-%d') + '}:HEAD '
+        xml = _run(command + self.path)
+        records = [entry for entry in self.process(xml, relative_url)]
+        columns = ['revision', 'author', 'date', 'textmods', 'kind',
+                   'action', 'propmods', 'path', 'message']
+        result = pd.DataFrame.from_records(records, columns=columns)
+        return result
 
 
 class BaseReport:
     """Base classes for the reports."""
 
-    def __init__(self, path, after=None):
+    def __init__(self, path, after=None,
+                 cloc_program=None, svn_program=None):
         """Initialization.
 
         :param str path: local repository to work with
         :param datetime.datetime after: limit how far back to mine SCM log.
+        :param str cloc_program: name of cloc program (e.g. cloc.pl)
+        :param str svn_program: name and location of svn client program.
 
         """
         self.path = path
         self.after = after
-        # Will holds the result report as a pandas.DataFrame.
-        self.data = None
-
-    def get_svn_log(self):
-        """Call svn log --xml -v and return the output as a DataFrame.
-
-        :param str path: path to execute svn log in.
-        :return: pandas.DataFrame
-
-        """
-        def process_entry(log_entry):
-            """Convert a single xml <logentry/> element to csv rows.
-
-            :param str log_entry: <logentry/> element.
-            :yield: one or more csv rows.
-
-            """
-
-            elem = ET.fromstring(log_entry)
-            revision = elem.attrib['revision']
-            values = {}
-            for sub in ['author', 'date', 'msg']:
-                try:
-                    values[sub] = elem.find(f'./{sub}').text
-                except (AttributeError, SyntaxError):
-                    log.warning('failed to retrieve %s in %s', sub, log_entry)
-                    values[sub] = None
-            if values['msg']:
-                values['msg'] = values['msg'].replace('\n', ' ')
-            for path_elem in elem.findall('*/path'):
-                other = {}
-                for sub in ['text-mods', 'kind', 'action', 'prop-mods']:
-                    try:
-                        other[sub] = path_elem.attrib[sub]
-                    except (AttributeError, SyntaxError, KeyError):
-                        other[sub] = None
-                try:
-                    path = path_elem.text
-                except (AttributeError, SyntaxError):
-                    path = None
-                row = (revision,
-                       values['author'],
-                       values['date'],
-                       other['text-mods'],
-                       other['kind'],
-                       other['action'],
-                       other['prop-mods'],
-                       path,
-                       values['msg'])
-                yield row
-
-        def process(xml):
-            """Convert output of svn log --xml -v to a csv.
-
-            :param iter(str) xml: iterable of string (one for each line)
-
-            :return: iter(tuple) containing the parsed entries.
-
-            """
-            log_entry = ''
-            for line in xml:
-                if line.startswith('<logentry'):
-                    log_entry += line
-                    continue
-                if not log_entry:
-                    continue
-                log_entry += line
-                if not line.startswith('</logentry>'):
-                    continue
-                yield from process_entry(log_entry)
-                log_entry = ''
-
-        command = 'svn log --xml -v '
-        if self.after:
-            command += '-r {' + self.after.strftime('%Y-%m-%d') + '}:HEAD '
-        xml = _run(command + self.path)
-        records = [entry for entry in process(xml)]
-        result = pd.DataFrame.from_records(records, columns=_DfSpec.columns)
-        return result
+        self.cloc_program = cloc_program or 'cloc'
+        self.svn_program = svn_program or 'svn'
 
     def get_log(self):
         """Forwards to get_svn_log."""
-        return self.get_svn_log()
+        collector = SvnLogCollector(self.path, after=self.after,
+                                    svn_program=self.svn_program)
+        return collector.get_log()
+
+    def get_cloc(self):
+        cmdline = f'{self.cloc_program} --csv --by-file {self.path}'
+        records = []
+        reader = csv.reader(_run(cmdline))
+        for record in reader:
+            if len(record) >= 5 and record[0]:
+                if record[0] != 'language':
+                    record[1] = str(pl.Path(record[1]))
+                    record[2:5] = [int(val) for val in record[2:5]]
+                records.append(record[:5])
+        columns = 'language,filename,blank,comment,code'.split(',')
+        return pd.DataFrame.from_records(records[1:], columns=columns)
+
+    def collect(self, **kwargs):
+        """Collect raw data from command line passed in kwargs."""
+        data = {}
+        for key, func in kwargs.items():
+            data[key] = func(self)
+        return data
 
 
 class AgeReport(BaseReport):
     """Reports files or components age in days."""
 
-    def __init__(self, path, after=None):
-        """See BaseReport.__init__ for options."""
-        super().__init__(path, after=after)
+    def __init__(self, path, **kwargs):
+        """See `BaseReport.__init__`."""
+        super().__init__(path, **kwargs)
 
-    def generate(self):
-        """Generate report from SCM data."""
+    def collect(self):
+        """Collect data needed for report."""
+        return super().collect(log=BaseReport.get_log)
+
+    def generate(self, data=None):
+        """Generate report from SCM data.
+
+        If data is not passed, calls self.get_log() to retrieve the raw
+        log from SCM.
+
+        :param pandas.DataFrame data: data overrides.
+
+        """
         columns = ['path', 'kind']
-        df1 = self.get_log()[columns + ['date']].copy()
+        if data is None:
+            data = self.collect()
+        assert 'log' in data
+        df = data['log'][columns + ['date']].copy()
         now = get_now()
-        df1['age'] = (now - pd.to_datetime(df1['date']))
-        df1 = df1[columns + ['age']].groupby(['path']).min().reset_index()
-        df1['age'] /= pd.Timedelta(1, unit='D')
-        self.data = df1
-        return self.data
+        df['age'] = (now - pd.to_datetime(df['date']))
+        df = df[columns + ['age']].groupby(['path']).min().reset_index()
+        df['age'] /= pd.Timedelta(1, unit='D')
+        return df
+
+
+class HotSpotReport(BaseReport):
+    """Identifies changing high complexity files."""
+
+    def __init__(self, path, **kwargs):
+        """See `BaseReport.__init__`."""
+        super().__init__(path, **kwargs)
+
+    def collect(self):
+        """Collect data necessary to the generation of the report.
+
+        :return: output of the SCM log and cloc identified by the keys log
+                 and cloc respectively
+        :rtype: dict(pandas.DataFrame)
+
+        """
+        return super().collect(log=BaseReport.get_log,
+                               cloc=BaseReport.get_cloc)
+
+    def generate(self, data=None):
+        """Generate report from SCM data.
+
+        If data is not passed, calls self.get_log() to retrieve the raw
+        log from SCM.
+
+        :param dict(pandas.DataFrame) data: data overrides.
+
+        """
+        if not data:
+            data = self.collect()
+        log = data['log']
+        if 'cloc' in data:
+            c_df = data['cloc'][['filename', 'code']]
+            c_df = c_df.rename(columns={'code': 'complexity'})
+        ch_df = log['path'].value_counts().to_frame('changes')
+        return pd.merge(c_df, ch_df, right_index=True, left_on='filename',
+                        how='outer')
+
+
--- tests.py
+++ tests.py
@@ -48,14 +48,16 @@ def add_data_frame_equality_func(test):
     test.addTypeEqualityFunc(pd.DataFrame, frame_equal)
 
 
-class SubversionTest(unittest.TestCase):
-    """Given svn command in $PATH environment variable."""
+class BaseReportTest(unittest.TestCase):
+    """Given a BaseReport instance."""
 
     def setUp(self):
         add_data_frame_equality_func(self)
         self.report = cm.BaseReport('.')
 
-    @mock.patch('codemetrics._run', return_value=textwrap.dedent('''
+    @mock.patch('codemetrics._run', side_effect=[
+        ['Relative URL: ^/project/trunk'],
+        textwrap.dedent('''
     <?xml version="1.0" encoding="UTF-8"?>
     <log>
     <logentry revision="1018">
@@ -63,56 +65,60 @@ class SubversionTest(unittest.TestCase):
     <date>2018-02-24T11:14:11.371061Z</date>
     <paths>
     <path text-mods="true" kind="file" action="M"
-       prop-mods="false">gstats.py</path>
+       prop-mods="false">/project/trunk/stats.py</path>
     <path text-mods="true" kind="file" action="M"
-       prop-mods="false">grequirements.txt</path>
+       prop-mods="false">/project/trunk/requirements.txt</path>
     </paths>
     <msg>Added joblib to requirements.txt</msg>
     </logentry>
     </log>
-    ''').split('\n'), spec=True)
+    ''').split('\n')], autospec=True)
     def test_get_log(self, call):
         """Simple svn call returns pandas.DataFrame."""
         expected = pd.read_csv(io.StringIO(textwrap.dedent('''
         revision,author,date,textmods,kind,action,propmods,path,message
-        1018,jlecomte,2018-02-24T11:14:11.371061Z,true,file,M,false,gstats.py,Added joblib to requirements.txt
-        1018,jlecomte,2018-02-24T11:14:11.371061Z,true,file,M,false,grequirements.txt,Added joblib to requirements.txt
+        1018,jlecomte,2018-02-24T11:14:11.371061Z,true,file,M,false,stats.py,Added joblib to requirements.txt
+        1018,jlecomte,2018-02-24T11:14:11.371061Z,true,file,M,false,requirements.txt,Added joblib to requirements.txt
         ''')), dtype='object')
         df = self.report.get_log()
         call.assert_called_with('svn log --xml -v .')
         self.assertEqual(df, expected)
 
-    @mock.patch('codemetrics._run', return_value=textwrap.dedent('''
+    @mock.patch('codemetrics._run', side_effect=[
+        ['Relative URL: ^/project/trunk'],
+        textwrap.dedent('''
     <?xml version="1.0" encoding="UTF-8"?>
     <log>
     <logentry revision="1018">
     <author>jlecomte</author>
     <date>2018-02-24T11:14:11.371061Z</date>
-    <paths> <path kind="file" action="M">gstats.py</path> </paths>
+    <paths><path kind="file" action="M">/project/trunk/stats.py</path></paths>
     <msg/>
     </logentry>
     </log>
-    ''').split('\n'), spec=True)
+    ''').split('\n')], autospec=True)
     def test_get_log_no_msg(self, call):
         """Simple svn call returns pandas.DataFrame."""
         expected = pd.read_csv(io.StringIO(textwrap.dedent('''
         revision,author,date,textmods,kind,action,propmods,path,message
-        1018,jlecomte,2018-02-24T11:14:11.371061Z,,file,M,,gstats.py,
+        1018,jlecomte,2018-02-24T11:14:11.371061Z,,file,M,,stats.py,
         ''')), dtype='object')
         df = self.report.get_log()
         call.assert_called_with('svn log --xml -v .')
         self.assertEqual(df, expected)
 
-    @mock.patch('codemetrics._run', return_value=textwrap.dedent('''
+    @mock.patch('codemetrics._run', side_effect=[
+        ['Relative URL: ^/project/trunk'],
+        textwrap.dedent('''
     <?xml version="1.0" encoding="UTF-8"?>
     <log>
     <logentry revision="1018">
     <date>2018-02-24T11:14:11.371061Z</date>
-    <paths> <path kind="file" action="M">stats.py</path> </paths>
+    <paths><path kind="file" action="M">/project/trunk/stats.py</path></paths>
     <msg>not much</msg>
     </logentry>
     </log>
-    ''').split('\n'), spec=True)
+    ''').split('\n')], autospec=True)
     def test_get_log_no_author(self, call):
         """Simple svn call returns pandas.DataFrame."""
         expected = pd.read_csv(io.StringIO(textwrap.dedent('''
@@ -123,6 +129,31 @@ class SubversionTest(unittest.TestCase):
         call.assert_called_with('svn log --xml -v .')
         self.assertEqual(df, expected)
 
+    @mock.patch('codemetrics._run', autospec=True)
+    def test_program_name(self, run):
+        """Test program_name taken into account."""
+        self.report.svn_program='svn-1.7'
+        self.report.get_log()
+        run.assert_called_with('svn-1.7 log --xml -v .')
+
+    @mock.patch('codemetrics._run', return_value=textwrap.dedent('''\
+    language,filename,blank,comment,code,"github.com/AlDanial/cloc..."
+    Python,.\\codemetrics.py,55,50,130
+    Python,.\\tests.py,29,92,109
+    Python,.\\setup.py,4,2,30
+    ''').split('\n'), autospec=True)
+    def test_get_cloc(self, run):
+        """Test handling of get_cloc output."""
+        actual = self.report.get_cloc()
+        run.assert_called_with('cloc --csv --by-file .')
+        expected = pd.read_csv(io.StringIO(textwrap.dedent('''
+        language,filename,blank,comment,code
+        Python,codemetrics.py,55,50,130
+        Python,tests.py,29,92,109
+        Python,setup.py,4,2,30
+        ''')))
+        self.assertEqual(actual, expected)
+
 
 class RepositoryTestCase(unittest.TestCase):
     """Given a repository of a few records."""
@@ -130,52 +161,86 @@ class RepositoryTestCase(unittest.TestCase):
     path = '.'
     log_df = pd.read_csv(io.StringIO(textwrap.dedent('''
     revision,author,date,textmods,kind,action,propmods,path,message
-    1016,jlecomte,2018-02-26T10:28:00Z,true,file,M,false,stats.py,modified again
-    1018,jlecomte,2018-02-24T11:14:11Z,true,file,M,false,stats.py,modified
-    1018,jlecomte,2018-02-24T11:14:11Z,true,file,M,false,requirements.txt,modified''')))
+    1016,jlecomte,2018-02-26T10:28:00Z,true,file,M,false,.\\stats.py,modified again
+    1018,jlecomte,2018-02-24T11:14:11Z,true,file,M,false,.\\stats.py,modified
+    1018,jlecomte,2018-02-24T11:14:11Z,true,file,M,false,.\\requirements.txt,modified''')))
+    cloc_df = pd.read_csv(io.StringIO(textwrap.dedent('''
+    language,filename,blank,comment,code,"github.com/AlDanial/cloc"
+    Python,.\\stats.py,28,84,100
+    Unknown,.\\requirements.txt,0,0,3
+    ''')))
 
     def setUp(self):
         add_data_frame_equality_func(self)
 
-    @mock.patch('codemetrics.get_now', spec=True,
+    @mock.patch('codemetrics.get_now', autospec=True,
                 return_value=pd.to_datetime(dt.datetime(2018, 2, 28)))
-    @mock.patch('codemetrics.BaseReport.get_log', spec=True, return_value=log_df)
-    def test_age_report(self, get_log, now):
+    @mock.patch('codemetrics.BaseReport.get_log', autospec=True,
+                return_value=log_df)
+    def test_age_report_raw_data(self, get_log, now):
         """Generate age by file report."""
         report = cm.AgeReport(self.path)
         report.generate()
-        expected = pd.read_csv(io.StringIO(textwrap.dedent('''
-        path,kind,age
-        requirements.txt,file,3.531817
-        stats.py,file,1.563889
-        ''')))
         get_log.assert_called()
         now.assert_called_with()
-        self.assertEqual(report.data, expected)
+        report.raw_data = self.log_df
 
-    @mock.patch('codemetrics._run', return_value=textwrap.dedent('''
+    @mock.patch('codemetrics.get_now', autospec=True,
+                return_value=pd.to_datetime(dt.datetime(2018, 2, 28)))
+    def test_age_report_with_custom_raw_data(self, now):
+        """Generate age by file report using specified raw_data."""
+        report = cm.AgeReport(self.path)
+        actual = report.generate({'log': self.log_df})
+        expected = pd.read_csv(io.StringIO(textwrap.dedent('''
+        path,kind,age
+        .\\requirements.txt,file,3.531817
+        .\\stats.py,file,1.563889
+        ''')))
+        now.assert_called_with()
+        self.assertEqual(actual, expected)
+
+    @mock.patch('codemetrics._run', side_effect=[
+        ['Relative URL: ^/project/trunk'],
+        textwrap.dedent('''
     <?xml version="1.0" encoding="UTF-8"?>
     <log>
     <logentry revision="1018">
     <date>2018-02-26T10:28:00Z</date>
-    <paths> <path kind="file" action="M">stats.py</path> </paths>
+    <paths><path kind="file" action="M">/project/trunk/stats.py</path></paths>
     <msg>not much</msg>
     </logentry>
     </log>
-    ''').split('\n'), spec=True)
-    @mock.patch('codemetrics.get_now', spec=True,
+    ''').split('\n')], autospec=True)
+    @mock.patch('codemetrics.get_now', autospec=True,
                 return_value=pd.to_datetime(dt.datetime(2018, 2, 28)))
     def test_age_report_with_time_limit(self, now, run):
         """Generate age by file report with a time limit on how far to go."""
         report = cm.AgeReport(self.path, after=dt.datetime(2018, 2, 26))
-        report.generate()
+        actual = report.generate()
         expected = pd.read_csv(io.StringIO(textwrap.dedent('''
         path,kind,age
         stats.py,file,1.563889
         ''')))
         run.assert_called_with('svn log --xml -v -r {2018-02-26}:HEAD .')
         now.assert_called_with()
-        self.assertEqual(report.data, expected)
+        self.assertEqual(actual, expected)
+
+    @mock.patch('codemetrics.BaseReport.get_log', autospec=True,
+                return_value=log_df)
+    @mock.patch('codemetrics.BaseReport.get_cloc', autospec=True,
+                return_value=cloc_df)
+    def test_hot_spot_report(self, get_cloc, get_log):
+        """Generate a report to find hot spots."""
+        report = cm.HotSpotReport(self.path, after=dt.datetime(2018, 2, 26))
+        actual = report.generate()
+        get_cloc.assert_called_with(report)
+        get_log.assert_called_with(report)
+        expected = pd.read_csv(io.StringIO(textwrap.dedent('''
+        filename,complexity,changes
+        .\\stats.py,100,2
+        .\\requirements.txt,3,1
+        ''')))
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
