50e34c7acd169e550ca1a6268ea032db5a34c984
elmotec <elmotec@gmx.com>
2018-05-13 21:41:56 -0400

    Bumped version to 0.6

--- codemetrics.py
+++ codemetrics.py
@@ -18,7 +18,7 @@ import pandas as pd
 import numpy as np
 import dateutil as du
 
-__version__ = '0.5.2'
+__version__ = '0.6'
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
