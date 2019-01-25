e3ca861ba840dc18fb293855307a095d6c70263b
elmotec <elmotec@gmx.com>
2019-01-25 07:15:11 -0500

    Bump version: 0.8.1 → 0.8.2

--- codemetrics/__init__.py
+++ codemetrics/__init__.py
@@ -6,7 +6,7 @@
 # noinspection SpellCheckingInspection
 __author__ = """Elmotec"""
 __email__ = 'elmotec@gmx.com'
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 
 from .core import *
 from .git import *
