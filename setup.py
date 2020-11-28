1400c445964661d3f6e0016b902258e98666c373
elmotec <1107551+elmotec@users.noreply.github.com>
2020-11-28 11:00:10 -0500

    Added support for Python 3.8, 3.9

--- setup.py
+++ setup.py
@@ -62,6 +62,8 @@ setuptools.setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development",
         "Topic :: Utilities",
         "Topic :: Software Development :: Version Control",
