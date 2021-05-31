796f22a380416696a257722571a3902809e524a4
elmotec <1107551+elmotec@users.noreply.github.com>
2021-05-31 11:32:16 -0400

    Bumped version to 0.11.4

--- codemetrics/__init__.py
+++ codemetrics/__init__.py
@@ -7,7 +7,7 @@
 # noinspection SpellCheckingInspection
 __author__ = """Elmotec"""
 __email__ = "elmotec@gmx.com"
-__version__ = "0.11.3"
+__version__ = "0.11.4"
 
 from .cloc import get_cloc
 from .core import (
