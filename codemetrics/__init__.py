a63e435eff988bc39e221bd9297c4f46759209db
elmotec <elmotec@gmx.com>
2022-04-24 14:38:33 -0400

    bump: version 0.11.6 → 0.11.7

--- codemetrics/__init__.py
+++ codemetrics/__init__.py
@@ -7,7 +7,7 @@
 # noinspection SpellCheckingInspection
 __author__ = """Elmotec"""
 __email__ = "elmotec@gmx.com"
-__version__ = "0.11.6"
+__version__ = "0.11.7"
 
 from .cloc import get_cloc
 from .core import (
